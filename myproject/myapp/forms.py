ccd6a6e3ac79cba4f359c6ac70b480125f117e35
bhanurathore231 <bhanurathore231@gmail.com>
2024-07-12 17:51:18 +0530

    update-code

---
+++ myproject/myapp/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+
+class CountrySearchForm(forms.Form):
+    search_term = forms.CharField(
+        max_length=100,
+        required=False,
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Search a country...'
+        })
+    )
+
+class RegionFilterForm(forms.Form):
+    regions = forms.ChoiceField(
+        choices=(
+            ('All', 'All'),
+            ('Africa', 'Africa'),
+            ('Americas', 'Americas'),
+            ('Antarctic', 'Antarctic'),
+            ('Asia', 'Asia'),
+            ('Europe', 'Europe'),
+            ('Oceania', 'Oceania'),
+        ),
+        required=False,
+        widget=forms.Select(attrs={'aria-label': 'Select region'}),
+    )
